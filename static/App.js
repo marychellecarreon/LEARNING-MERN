4a67896c1494635d616ac096d5cb3a68e66b607b
Mary Chelle Carreon <marychellecarreon@gmail.com>
2017-10-31 16:34:21 +0800

    starter files

---
+++ static/App.js
@@ -0,0 +1,7 @@
+var contentNode = document.getElementById('contents');
+var component = React.createElement(
+  'h1',
+  null,
+  'Hello, Mary Chelle Carreon!'
+); //A simple JSX component
+ReactDOM.render(component, contentNode); //Render the component inside the content Node
